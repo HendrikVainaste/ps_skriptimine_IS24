7cfcb35fcacd3cf906c6a3d8435dcdd7d07d170e
HendrikVainaste <hendrik.vainaste@voco.ee>
2025-02-02 19:23:53 +0200

    Notepadi töötamine

---
+++ Praktika8.ps1
@@ -0,0 +1,12 @@
+﻿# Jooksva protsessi kontrollimine 
+
+	while(Get-Process | where{$_.ProcessName -eq "notepad"}){
+
+# Teade, et Notepad töötab    
+
+	Write-Host "Notepad is running" -ForegroundColor Green
+
+# Pausi tegemine 2 sekundiks
+
+    		Start-Sleep -Seconds 2
+}
