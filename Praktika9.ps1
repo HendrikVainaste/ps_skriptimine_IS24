3c8aed30d40b3e22e3448a8848220ca60b29927d
HendrikVainaste <hendrik.vainaste@voco.ee>
2025-02-02 19:26:10 +0200

    Kui kaua Notepad töötab

---
+++ Praktika9.ps1
@@ -0,0 +1,12 @@
+	$count = 0
+
+	do {
+    		Write-Host "Notepad is running" -ForegroundColor Green
+    		$count++
+    		Start-Sleep -Seconds 1
+		} 
+
+
+	while(Get-Process | where{$_.ProcessName -eq "notepad"})
+
+	Write-Host "$count"
